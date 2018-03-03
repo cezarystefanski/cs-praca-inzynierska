cdb61e5f32c913ced37cea78c6926c7381bf3198
cezarystefanski <cezarystefanski@gmail.com>
2018-03-03 12:46:58 +0100

    initial webpack config

---
+++ src/App.react.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React App starting point</h1>
+      <h2>Initial stuff</h2>
+    </div>
+  );
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
