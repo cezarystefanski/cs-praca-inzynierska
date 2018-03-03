a085e0b9abdb3402cf09409bdc178e1bb2ea8da9
cezarystefanski <cezarystefanski@gmail.com>
2018-03-03 16:49:11 +0100

    added stylelint and eslint configs

---
+++ config/development.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const config = {
+  entry: path.resolve(__dirname, '..', './src/client/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'main.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, '..', 'src/client'),
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/client/index.html',
+      filename: './index.html'
+    })
+  ]
+};
+
+module.exports = config;
---
+++ src/client/App.react.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './App.scss';
+
+const App = () => (
+  <div>
+    <h1 className={classes.heading}>React App starting point</h1>
+    <h2>Initial stuff</h2>
+  </div>
+);
+
+export default App;
